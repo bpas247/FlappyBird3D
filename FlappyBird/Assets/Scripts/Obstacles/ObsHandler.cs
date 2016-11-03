fc503ffb56173ebaf42c53b5ad6ad4af41a25d8b
bpas247 <bpas247@hotmail.com>
2016-11-03 12:59:04 -0700

    Added some nice C#Scripts. FILL THEM OUT MEOW!!!!!!

---
+++ FlappyBird/Assets/Scripts/Obstacles/ObsHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObsHandler : MonoBehaviour {
+
+
+    public double moveSpeed = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (transform.position.x > -5)
+			transform.Translate (new Vector3 (-1 * 2, 0, 0) * Time.deltaTime);
+		else
+			GameObject.Destroy (this);
+	}
+}
